90d69440af188be4112adbecc24063a35ceca54f
phihd <phictin@gmail.com>
2020-11-09 21:47:54 +0200

    create Item class

---
+++ src/Item.cpp
@@ -0,0 +1,8 @@
+#include "Item.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Item::Item(const string &name): name_(name) {}
---
+++ src/Item.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Item {
+
+private:
+    string name_;
+
+public:
+    Item(const string &name_);
+
+};
