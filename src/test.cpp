b2db553bbee2ce2ad8e5d1a765e250e31df796de
Taige Wang <taige.wang@aalto.fi>
2020-11-26 16:49:04 +0200

    Small fixes

--- src/test.cpp
+++ src/test.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-using namespace std;
-/* A test instance*/
-int testInstance1()
-{
-   if (7 == 9)
-      return 1;
-   else
-      return 0;
-}
+#include <dungeon/Item.hpp>
 
-/* A temporary testing solution built on CTest. 
- * This needs to be changed due to limited documentation. 
- * Also, this method is not practical. 
- */
-int main()
+TEST_CASE("", "")
 {
-   /* The return code for this test instance. 0=pass*/
-   int s = 0;
-   s |= testInstance1(); // Whenever a test failed, the status code got changed to a non-zero value
-   s |= 7 == 9;          // This does not work as expected because `false` is evaluated to 0
-   return s;
+   SECTION("")
+   {
+      REQUIRE(1 == 1);
+   }
 }
