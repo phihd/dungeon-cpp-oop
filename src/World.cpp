b11d47be275c9f9375f1f9f65a894653d2ca6b60
Khoa Nguyen <khoa.nguyen@aalto.fi>
2020-12-09 02:37:21 +0200

    Minor fix to World.cpp

--- src/World.cpp
+++ src/World.cpp
@@ -21,32 +21,32 @@ std::vector<Room> Game::CreateWorld()
 		std::vector<string> design = lv_designs[i];
 		std::vector<Item> treasure_list = {};
 		if (i == 0 || i == 1) {
-			auto seed = rand();
-			auto rng = std::default_random_engine{};
+			seed = rand();
+			rng = std::default_random_engine{};
 			rng.seed(seed);
 			std::shuffle(std::begin(temp_items[0]), std::end(temp_items[0]), rng);
 			temp_items[0].resize(5);
 			std::vector<Item> treasure_list = temp_items[0];
 		}
 		else if (i == 3 || i == 4) {
-			auto seed = rand();
-			auto rng = std::default_random_engine{};
+			seed = rand();
+			rng = std::default_random_engine{};
 			rng.seed(seed);
 			std::shuffle(std::begin(temp_items[1]), std::end(temp_items[1]), rng);
 			temp_items[0].resize(5);
 			std::vector<Item> treasure_list = temp_items[1];
 		}
 		else if (i == 5 || i == 7) {
-			auto seed = rand();
-			auto rng = std::default_random_engine{};
+			seed = rand();
+			rng = std::default_random_engine{};
 			rng.seed(seed);
 			std::shuffle(std::begin(temp_items[2]), std::end(temp_items[2]), rng);
 			temp_items[0].resize(5);
 			std::vector<Item> treasure_list = temp_items[2];
 		}
 		else if (i == 8) {
-			auto seed = rand();
-			auto rng = std::default_random_engine{};
+			seed = rand();
+			rng = std::default_random_engine{};
 			rng.seed(seed);
 			std::shuffle(std::begin(temp_items[3]), std::end(temp_items[3]), rng);
 			temp_items[0].resize(5);
@@ -59,8 +59,8 @@ std::vector<Room> Game::CreateWorld()
 		new_room.SetAllySpawn(spawn);
 		//Set spawn for enemies and randomize enemies
 		srand(time(0));
-		auto seed = rand();
-		auto rng = std::default_random_engine{};
+		seed = rand();
+		rng = std::default_random_engine{};
 		rng.seed(seed);
 		if ((i == 2 || i == 7 || i == 9)) {
 			new_room.SetEnemySpawn(boss_spawns[i]);
