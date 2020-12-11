db6136133a1118a659507fa9c91b49bcbabf4881
Mai Vu <mai.vu@aalto.fi>
2020-12-11 15:04:27 +0200

    Minor changes

--- src/Stat.cpp
+++ src/Stat.cpp
@@ -15,7 +15,7 @@ Stat& Stat::operator+=(const Stat &b) {
     this->HP_ += b.HP_;
     this->maxHP_ += b.maxHP_;
     this->attackRange_ += b.attackRange_;
-    this->moveRange_ += b.attackRange_;
+    this->moveRange_ += b.moveRange_;
     if (b.maxHP_ > 0)
         this->HP_ += b.maxHP_;
 
@@ -31,6 +31,8 @@ Stat& Stat::operator-=(const Stat &b) {
     this->def_ -= b.def_;
     this->HP_ -= b.HP_;
     this->maxHP_  -= b.maxHP_;
+    this->attackRange_ += b.attackRange_;
+    this->moveRange_ += b.moveRange_;
     if (b.maxHP_ > 0)
         this->HP_ += b.maxHP_;
 
